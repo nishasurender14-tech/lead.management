395a067816754074eeb025332e3fa5efb992b0e9
nishasurender14-tech <nishasurender14@gmail.com>
2026-08-20 16:16:31 +0530

    Point Android app to KIP Financial CRM V4

--- android/app/src/main/java/com/kipconsultancy/leadmanagement/MainActivity.java
+++ android/app/src/main/java/com/kipconsultancy/leadmanagement/MainActivity.java
@@ -3,6 +3,7 @@ package com.kipconsultancy.leadmanagement;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -17,6 +18,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         webView = new WebView(this);
         setContentView(webView);
+
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
@@ -25,14 +27,30 @@ public class MainActivity extends Activity {
         settings.setAllowContentAccess(true);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
+        settings.setLoadWithOverviewMode(false);
+        settings.setUseWideViewPort(true);
+
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
-        webView.loadUrl("https://nishasurender14-tech.github.io/lead.management/");
+        webView.loadUrl("https://nishasurender14-tech.github.io/lead.management/kip-financial-crm-v4.html");
     }
 
     @Override
     public void onBackPressed() {
         if (webView != null && webView.canGoBack()) webView.goBack();
         else super.onBackPressed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.loadUrl("about:blank");
+            webView.stopLoading();
+            webView.destroy();
+        }
+        super.onDestroy();
     }
 }
